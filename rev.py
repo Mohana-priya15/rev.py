b7b8157a9390f311744fab494564a1d7407913f2
Mohana-priya15 <mpmsc1415@gmail.com>
2024-05-26 12:20:16 +0530

    initial commit

---
+++ rev.py
@@ -0,0 +1,6 @@
+mystr=input("Enter the String:")
+rev= mystr[::-1]
+if mystr == rev:
+    print("Entered String ",mystr,"is Palindrome")
+else:
+    print("Entered String",mystr,"is not Palindrome")    
